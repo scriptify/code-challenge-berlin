4d00943c09d298d7d2b695d146fdd3f835a0f1e4
Emily <emily.a.morgan00@gmail.com>
2018-04-13 00:55:25 +0200

    Style second Enter Data page

--- src/components/EnterData/index.js
+++ src/components/EnterData/index.js
@@ -1,69 +1,83 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom"
-
+import selfie from '../../take-selfie.png';
+import React, {
+    Component
+}
+from "react";
+import {
+    Link
+}
+from "react-router-dom"
 class EnterData extends Component {
-  constructor(props) {
-    super(props);
-    console.log(window.meUser)
-    if (!window.meUser) {
-
-        this.state = {
-            attributes: [
-                {
-                  name: "name",
-                  value: ""
-                },
-                {
-                  name: "age",
-                  value: ""
-                },
-                {
-                  name: "country",
-                  value: ""
-                },
-                {
-                  name: "description",
-                  value: ""
+    constructor(props) {
+        super(props);
+        console.log(window.meUser)
+        if (!window.meUser) {
+            this.state = {
+                attributes: [
+                    {
+                        name: "name"
+                        , value: ""
+                }
+                    , {
+                        name: "age"
+                        , value: ""
+                }
+                    , {
+                        name: "country"
+                        , value: ""
+                }
+                    , {
+                        name: "description"
+                        , value: ""
                 }
               ]
-          }
-    } else {
-        this.state = {attributes: window.meUser}
-    }
-  }
-
-  changeAtt(name, value) {
-    this.setState({
-        ...this.state, attributes: this.state.attributes.map((a) => {
-            if (a.name === name) {
-                return {
-                    ...a, value
-                }
             }
-            return a
+        }
+        else {
+            this.state = {
+                attributes: window.meUser
+            }
+        }
+    }
+    changeAtt(name, value) {
+        this.setState({...this.state, attributes: this.state.attributes.map((a) => {
+                if (a.name === name) {
+                    return {...a, value
+                    }
+                }
+                return a
+            })
         })
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Enter some details about you</h1>
-        <form >
-            {this.state.attributes.map((att) => {
-                return (
-                    <div key={att.name}>
-                        <input type="text" value={att.value} onChange={(e) => {this.changeAtt(att.name, e.target.value)}}/>
-                        <label>{att.name}</label>
-                    </div>
-                )
-            })}
-            <Link to="/preview"><button onClick={() => {window.meUser = this.state.attributes}}>Go</button></Link>
-            
-        </form>
-      </div>
-    );
-  }
+    }
+    render() {
+        return ( < div className = "EnterData row" > < div className = " col-xs-12" > < img src = {
+                selfie
+            }
+            className = "Selfie"
+            alt = "logo" / > < /div>< a href = "#"
+            className = "col-xs-12" > Import Photo < /a> < form className="col-xs-12"> {
+            this.state.attributes.map((att) => {
+                return ( < div key = {
+                        att.name
+                    } > < input type = "text"
+                    placeholder = {
+                        att.name
+                    }
+                    value = {
+                        att.value
+                    }
+                    onChange = {
+                        (e) => {
+                            this.changeAtt(att.name, e.target.value)
+                        }
+                    }
+                    /> < /div > )
+            })
+        } < p class = "describeYou" > Add 3 things that describe you < /p>< Link to = "/preview
+        " > < button onClick = { () => {
+        window.meUser = this.state.attributes
+    }
+} > + < /button></Link > < /form> <hr / > < /div > );
 }
-
+}
 export default EnterData;
