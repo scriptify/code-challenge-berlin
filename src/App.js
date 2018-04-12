dbf9ddc9d25b04e295566b4c3aa5afff5f059b61
Emily <emily.a.morgan00@gmail.com>
2018-04-12 23:26:50 +0200

    Add assets

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, {
     Component
 }
 from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import 'normalize.less'
 class App extends Component {
     render() {
-        return ( < div className = "App" > < header className = "App-header" > < img src = {
+        return ( < div className = "App" > < header className = "App-header row middle-xs center-xs" > < div className = "App-header-contain" > < img src = {
                 logo
             }
-            className = "App-logo"
-            alt = "logo" / > < h1 className = "App-title" > Welcome to React < /h1> < /header > < p className = "App-intro" > To get started, edit < code > src / App.js < /code> and save to reload. < /p > < /div>);
-        }
+            className = "App-logo col-xs-12"
+            alt = "logo" / > < h1 className = "App-title col-xs-12" > Hello please write your code < /h1>< /
+            div > < /header >  < p className = "App-intro" > Questions? Do not hesitate to <a href="#" class="contact-us">contact us! </a > < /p > < /div > );
     }
-    export default App;
+}
+export default App;
