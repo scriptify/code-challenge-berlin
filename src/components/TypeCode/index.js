cfcf4deb6019aa3f7d62a0f618d8ba31017b6d7d
Emily <emily.a.morgan00@gmail.com>
2018-04-12 23:44:33 +0200

    Restly pages after structural changes

--- src/components/TypeCode/index.js
+++ src/components/TypeCode/index.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import logo from '../../logo.png';
 
 class TypeCode extends Component {
 
 
     render() {
         return (
-            <div>
-                <h1>Enter Code</h1>
-                <input type="text"/>
-                <button onClick={() => {window.location.href = "/enter-data"}}>Join</button>
-            </div>
+         < div className = "App" >
+         	< header className = "App-header row middle-xs center-xs" >
+         		< div className = "App-header-contain" > 
+	        		< img src = { 
+						 logo 
+			            } 
+			            className = "App-logo col-xs-12" 
+			            alt = "logo" / >
+	            	< h1 className = "App-title col-xs-12" > Hello please write your code < /h1>
+	        	< / div >
+	        < /header >
+	        < p className = "App-intro" > Questions? Do not hesitate to
+	        	<a href="#" class="contact-us">contact us! </a >
+	        < /p > 
+	   
+            <input type="text"/>
+            <button onClick={() => {window.location.href = "/enter-data"}}>Join</button>
+        </div>
         );
     }
 }
