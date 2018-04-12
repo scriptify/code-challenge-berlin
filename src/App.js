5ed0f8930e9e6e41920dd1c07f98c219202bdcd9
timoangerer <timo.forum@yahoo.com>
2018-04-12 23:22:16 +0200

    add stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TypeCode from "./components/TypeCode"
+import Preview from "./components/Preview"
+import EnterData from "./components/EnterData"
+import UserList from "./components/UserList"
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <Route exact path="/" render={() => <TypeCode/>}></Route>
+      <Route path="/enter-data" render={() => <EnterData/>} />
+      <Route path="/preview" render={() => <Preview />} />
+      <Route path="/groups" render={() => <UserList/>} />
       </div>
     );
   }
