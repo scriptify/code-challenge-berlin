d11bdc44261ef7b9aa1b7dc70752ed135d1c36a8
Emily <emily.a.morgan00@gmail.com>
2018-04-13 09:42:59 +0200

    Debug preview 2

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -11,24 +11,7 @@ import {
 from "react-router-dom"
 
 /*
-                    {
-                        window.meUser && window.meUser.map((att) => {
-                                return ( < div key = {
-                                        att.name
-                                    } > < p > {
-                                        att.name
-                                    } < /p> < p > {
-                                    att.value
-                                } < /p> < /div > )
-                        })
-                    }
-*/
-
-class Preview extends Component {
-    render() {
-        return ( 
-            <div className = "preview">
-                <div className = "bg"></div>
+<div className = "bg"></div>
                 <div className = "App-header-contain row middle-xs center-xs"> 
                     <img src = { logo } className = "col-xs-3" alt = "logo" />
                     <h1 className = "col-xs-9"> Profile preview </h1>
@@ -46,8 +29,19 @@ class Preview extends Component {
                     }
                     <Link to = "/enter-data"><img src = { pencil } className = "col-xs-12 pencil"  alt = "pencil" />< /Link >
                     <Link to = "/groups"> <button className = "saveProfile "> Save Profile </button></Link >
-                </div>
-            );
+*/
+
+class Preview extends Component {
+    constructor(props) {
+        super(props);
+        console.log(window.meUser)
+    }
+    render() {
+        return ( 
+            <div className="preview">
+                User: {JSON.stringify(window.meUser)}    
+            </div>
+        );
 }
 }
 export default Preview;
