e2cfeb8d008bc060a6c08ea87d30ca8daba3fb4d
Emily <emily.a.morgan00@gmail.com>
2018-04-13 02:42:27 +0200

    Style preview and group pages - unfinished

--- src/components/Preview/index.js
+++ src/components/Preview/index.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom"
-
+import pencil from '../../pencil.png';
+import logo from '../../logo.png';
+import profilepic from '../../profilepic.png';
+import React, {
+    Component
+}
+from 'react';
+import {
+    Link
+}
+from "react-router-dom"
 class Preview extends Component {
     render() {
-        return (
-            <div>
-                {window.meUser && window.meUser.map((att) => {
-                    return (
-                        <div key={att.name}>
-                            <p>{att.name}</p>
-                            <p>{att.value}</p>
-                        </div>
-                    )
-                })}
-                <Link to="/enter-data"><button>Edit</button></Link>
-                <Link to="/groups"><button>Join Group</button></Link>
-            </div>
-        );
+        return ( < div className = "preview" > < div class = "bg" > < /div>< div class = "App-header-contain row middle-xs center-xs" > < img src = {
+            logo
+        }
+        className = "col-xs-3"
+        alt = "logo" / > < h1 className = "col-xs-9" > Profile preview < /h1>< /div > < img src = {
+            profilepic
+        }
+        className = "profilepic col-xs-12"
+        alt = "profilepic" / > {
+            window.meUser && window.meUser.map((att) => {
+                    return ( < div key = {
+                            att.name
+                        } > < p > {
+                            att.name
+                        } < /p> < p > {
+                        att.value
+                    } < /p> < /div > )
+            })
+    } < Link to = "/enter-data" > < img src = {
+        pencil
     }
+    className = "col-xs-12 pencil"
+    alt = "pencil" / > < /Link > < Link to = "/groups
+    " > < button className = "
+    saveProfile " > Save Profile < /button></Link > < /div>);
 }
-
+}
 export default Preview;
--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,60 +1,64 @@
-import React, { Component } from "react";
+import React, {
+    Component
+}
+from "react";
+import logo from '../../logo.png';
+import profilethumb from '../../profilethumb.png';
+import flag from '../../flag.png';
+
 function createUser() {
-  return [
-    {
-      name: "name",
-      value: "Felix"
+    return [
+        {
+            name: "age"
+            , value: "17"
+    }
+        , {
+            name: "name"
+            , value: "Felix"
     },
-    {
-      name: "age",
-      value: "17"
-    },
-    {
-      name: "country",
-      value: "Italy"
-    },
-    {
-      name: "description",
-      value: "420 blaze it"
+
+        {
+            name: "country"
+            , value: "Italy"
     }
   ];
 }
-
 class UserList extends Component {
-  constructor(props) {
-    super(props);
-
-    let arr = [];
-
-    if (window.meUser) {
-      arr = [window.meUser];
+    constructor(props) {
+        super(props);
+        let arr = [];
+        let isAge = false;
+        if (window.meUser) {
+            arr = [window.meUser];
+        }
+        this.state = {
+            users: [createUser(), ...arr]
+        };
     }
-
-    this.state = {
-      users: [createUser(), ...arr]
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        {this.state.users.map(u => {
-          return (
-            <ul key={u[0].value}>
-              {u.map(att => {
-                return (
-                  <li key={att.name}>
-                    <p>{att.name}</p>
-                    <p>{att.value}</p>
-                  </li>
-                );
-              })}
-            </ul>
-          );
-        })}
-      </div>
-    );
-  }
-}
-
-export default UserList;
+    render() {
+        return ( < div className = "groupPage" > < div class = "App-header-contain row middle-xs center-xs" > < img src = {
+                logo
+            }
+            className = "col-xs-12"
+            alt = "logo" / > < /div>< div className = "listItems row middle-xs center-xs" >< img src = {
+            profilethumb
+        }
+        className = "col-xs-3"
+        alt = "logo" / > < div className = "listItem col-xs-9" > {
+            this.state.users.map(u => {
+                    return ( < ul key = {
+                            u[0].value
+                        } > {
+                            u.map(att => {
+                                return ( < li key = {
+                                    att.name
+                                } > < p > {
+                                    att.value
+                                } < /p> < /li > );
+                            })
+                        } < /ul>);
+                    })
+            } < /div></div > < /div > );
+        }
+    }
+    export default UserList;
